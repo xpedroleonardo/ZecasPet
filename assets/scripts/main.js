1a04067b2b75952466a59a7f0a436adcdb6c024a
xpedroleonardo <aispedroleonardo@gmail.com>
2021-05-03 17:03:24 -0300

    alterando pastas e criando modal

--- assets/scripts/main.js
+++
@@ -1,9 +0,0 @@
-function showTitle(id) {
-  var title = document.getElementById('Title' + id);
-  title.classList.add("show")
-}
-
-function closeTitle(id) {
-  var title = document.getElementById('Title' + id);
-  title.classList.remove("show");
-}
